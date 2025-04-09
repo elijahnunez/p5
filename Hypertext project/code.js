5125b857a33f412e46776fe374be5c052d843bd3
elijahnunez <146300849+elijahnunez@users.noreply.github.com>
2025-04-09 10:34:24 -0400

    commit to main

--- Hypertext project/code.js
+++ Hypertext project/code.js
@@ -47,19 +47,3 @@ bounceTrigger.addEventListener("click", () => {
     intervals.push(interval);
   }
 });
-
-
-const openButton = document.getElementById('openPopup');
-const popupContainer = document.getElementById('popupContainer');
-const popupContent = document.querySelector('.popup-content');
-const closeButton = document.querySelector('.close-button');
-
-openButton.addEventListener('click', () => {
-  popupContainer.classList.add('show');
-  popupContent.classList.add('show');
-});
-
-closeButton.addEventListener('click', () => {
-  popupContainer.classList.remove('show');
-  popupContent.classList.remove('show');
-});
